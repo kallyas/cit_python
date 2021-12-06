b5498058a674b9ffc16d06dae5d6ccae063afa9a
kallyas <kallyasmedia@gmail.com>
2021-12-06 15:42:47 +0000

    assignment

---
+++ assignment_6_12_2021.py
@@ -0,0 +1,59 @@
+# 1. create a list and find the average of that list using numpy
+
+import numpy as np
+
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+avg = np.mean(num_list)
+print(avg)
+
+# 2. create a nested dictionary
+def nested_dict():
+    nested_dict = {'key1': {'key2': {'key3': 'value'}}}
+    print(nested_dict)
+
+nested_dict()
+
+# 3. create a list and find the median value
+num_list_2 = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+median = np.median(num_list_2)
+print(median)
+
+# 4. what is the output of the following code?
+# l = [None] * 10
+# print(len(l))
+10
+
+# 5. Briefly explain Bubble Sort and Insertion Sort and selection sort algorithms.
+# Bubble sort is a sorting algorithm that compares each pair of adjacent items and swaps them if they are in the wrong order.
+# Insertion sort is a sorting algorithm that builds the final sorted array (or list) one item at a time.
+# Selection sort is a sorting algorithm that sorts the items by selecting the next item each time and putting it in the correct position.
+
+# 6. What is the output of the following code?
+# aList = [5, 10, 15, 25]
+# print(aList[::-2])
+[25, 10]
+
+# 7. What is the output of the following code?
+my_list = ["Hello", "Python"]
+print("-".join(my_list))
+# Hello-Python
+
+# 8. A python tuple can also be created using paranthesis(True or False)
+# True
+
+# 9. What is the output of the following code?
+# aTuple = "Yellow", 20 , "Red"
+# a,b,c = aTuple
+# print(a)
+
+# Yellow
+
+# 10. Please select the answer in the code to empty the following dictionary
+# student = {
+#     "name": "Emma",
+#     "class": 9,
+#     "marks": 75
+# }
+
+# student.clear()
+
